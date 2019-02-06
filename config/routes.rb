011da5add342114f37cb737c49e29ce2113e26ca
buffalorob <peckham.rob@gmail.com>
2019-02-06 12:30:28 -0500

    namespaced routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  namespace :api do
+    namespace :v1 do
+      devise_for :users
   root to: "home#index"
+    end
+  end
 end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
