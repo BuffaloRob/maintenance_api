8caa42b90e7af8a4397d9c540a8b4da9d8ecd39f
buffalorob <peckham.rob@gmail.com>
2019-02-06 12:34:05 -0500

    modded default devise paths for registering, logging in and logging out

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      devise_for :users
+      devise_for :users,
+      path: '',
+      path_names: {
+        sign_in: 'login',
+        sign_out: 'logout',
+        registration: 'signup'
+      },
+      controllers: {
+        sessions: 'sessions',
+        registrations: 'registrations'
+      }
   root to: "home#index"
     end
   end
