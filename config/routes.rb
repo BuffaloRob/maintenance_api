d5c4959426bcffd9320cfae5eb3920d09614dc0b
buffalorob <peckham.rob@gmail.com>
2019-02-07 12:59:39 -0500

    routes: - custom naming for devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      devise_for :users,
-      path: '',
-      path_names: {
-        sign_in: 'login',
-        sign_out: 'logout',
-        registration: 'signup'
-      },
-      controllers: {
-        sessions: 'sessions',
-        registrations: 'registrations'
-      }
-  root to: "home#index"
+      devise_for :users
+      root to: "home#index"
     end
   end
 end
