446ec4bf7189af88ac127848037c8a9bc0fa0dda
buffalorob <peckham.rob@gmail.com>
2019-02-08 10:09:09 -0500

    devise_config: + warden strategy for JSON web tokens

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,18 +10,7 @@ Devise.setup do |config|
   # by default. You can change it below and use your own secret key.
   # config.secret_key = '6be2c5a6bf434863b820c2de9fa08582d0cf2b492d8781a9ac42c08cbbc52dae79e43d5b9809e976d87bd6d1c50e7921f2fa68c489df210201f1d7a67d83135f'
 
-  config.jwt do |jwt|
-    jwt.secret = ENV['DEVISE_JWT_SECRET_KEY']
-    jwt.dispatch_requests = [
-      ['POST', %r{^/login$}]
-    ]
-    jwt.revocation_requests = [
-      ['DELETE', %r{^/logout$}]
-    ]
-    jwt.expiration_time = 1.day.to_i 
-  end 
-
-  config.navigational_formats = []
+  
   
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
@@ -275,10 +264,11 @@ Devise.setup do |config|
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
   #
-  # config.warden do |manager|
-  #   manager.intercept_401 = false
-  #   manager.default_strategies(scope: :user).unshift :some_external_strategy
-  # end
+  config.warden do |manager|
+    manager.strategies.add(:jwt, Devise::Strategies::JsonWebToken)
+
+    manager.default_strategies(scope: :user).unshift :jwt
+  end
 
   # ==> Mountable engine configurations
   # When using Devise inside an engine, let's call it `MyEngine`, and this engine
