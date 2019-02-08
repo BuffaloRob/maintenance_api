b54a5145aefec0daeb6b159e18124a1f32d34508
buffalorob <peckham.rob@gmail.com>
2019-02-08 14:15:53 -0500

    cors: changed to closer match the devise-jwt docs

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,13 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000'
+    # TODO change origin to front end address
+    origins 'http://localhost:3000'
 
     resource '*',
-      headers: :any,
-      expose: ['Authorization'],
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      headers: %w(Authorization),
+      expose: %w(Authorization),
+      methods: :any,
+      max_age: 600
   end
 end
