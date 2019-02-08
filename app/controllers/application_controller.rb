94595b5212bc119192442525b0e522977672a318
buffalorob <peckham.rob@gmail.com>
2019-02-08 14:14:46 -0500

    app controller: + commented out code for future use

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
   include ActionController::MimeResponds
+  #  before_action :authenticate_user!
   respond_to :json
   
   def render_resource(resource)
